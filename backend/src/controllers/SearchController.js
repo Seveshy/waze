30cb4ccd7ac52f4ebe2fec85af3be5accb610203
Daniel Major <danielalvesmajor@outlook.com>
2020-01-20 12:18:43 -0300

    Finalização do backend

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -1,28 +1,33 @@
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('./utils/parseStringAsArray');
 
+// Buscar todos os devs num raio de 10km
+// Filtrar por tecnolgias
+
 module.exports = {
     async index(req, res) {
-        const { latitude, longitude, techs } = req.query;
+     const { latitude, longitude, techs } = req.query;
 
-        const techsArray = parseStringAsArray(techs);
+     const techsArray = parseStringAsArray(techs);
 
-        const devs = await Dev.find({
-            techs: {
-                $in: techsArray
-            },
-            location: {
-                $near: {
-                    $geometry: {
-                        type: 'Point',
-                        coordinates: [longitude, latitude],
-                    },
-                    $maxDistance: 10000,
+     const devs = await Dev.find({
+        techs: {
+            $in: techsArray, 
+        },
+
+    /** Retirar esse código para retornar dev */
+        location: {
+            $near: {
+                $geometry: {
+                    type: 'Point',
+                    coordinates: [longitude, latitude],
                 },
-            },
-        });
-        // Buscar todos os devs num raio de 10km
-        
+                $maxDistance: 10000
+            }
+        }
+        /** ************** */
+     });
+
         return res.json({ devs });
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,8 @@ const app = express();
 
 mongoose.connect('mongodb+srv://seveshy:852456@cluster0-xtf84.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
 
 app.use(express.json());
